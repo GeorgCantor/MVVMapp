ce9822d8c2dfd21aa89ae5fbe12034761f0d3467
GeorgCantor <cupsman1986@gmail.com>
2020-04-10 12:38:00 +0300

    init commit

---
+++ app/src/main/java/com/example/mvvmapp/MyApplication.kt
@@ -0,0 +1,21 @@
+package com.example.mvvmapp
+
+import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+class MyApplication : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppInjector.init(this)
+    }
+
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
+}
